79474c726890192cceafae09ddd9d2188ce693c3
BennoAlif <bennoalif41@gmail.com>
2019-10-08 22:09:21 +0700

    Init

---
+++ src/com/benno/Main.java
@@ -0,0 +1,16 @@
+package com.benno;
+    /*
+        NAMA        : BENNO ALIF ANGGARA
+        KELAS       : IF1
+        NIM         : 10118024
+        Deskripsi   : Memasukkan nilai dari keyboard
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);
+        }
+    }
+}
